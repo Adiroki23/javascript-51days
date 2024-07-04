1630eca8c477bb5321fdb58bda0a8463477016c0
Aditi Srivastava <117526497+Adiroki23@users.noreply.github.com>
2024-07-04 07:29:44 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12323423 
+let accountEmail = "aditi@google.com"
+var accountPassword = "126436"
+accountCity = "nepal"
+let accountState;
+
+
+// accountId = 2 // not allowed
+
+accountEmail = "adi@hc.com"
+accountPassword = "546374623"
+accountCity = "Bhopal"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+//alert (3+3) // we are using node js not brouser
+
+console.log (3 + 3);
+console.log("Hitesh")
+
+
+let name ="hitesh"
+
+let  age = 18
+
+let isLoggedIn = false
+
+// number => to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => not assigned value
+//symbol => unique
+
+
+//object
+
+console.log(typeof null); //output =>object
+console.log(typeof undefined); //output => undefined
