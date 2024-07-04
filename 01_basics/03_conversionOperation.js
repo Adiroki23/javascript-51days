f6f6aa2a817cb17c8b76155de70607bf85d34810
Aditi Srivastava <117526497+Adiroki23@users.noreply.github.com>
2024-07-04 09:22:10 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN
+//TRUE => 1; FALSE => 0;
+
+
+let isLoggedIn = "adiroki"
+
+let booleanIsLoogedIn =Boolean(isLoggedIn)
+console.log(booleanIsLoogedIn);
+
+// 1 => true; 0 => false
+//"" => fasle
+//"adiroki" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
