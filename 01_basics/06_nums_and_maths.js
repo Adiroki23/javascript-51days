ade9d2ccfb72d832fc59b04f989aecb30d3bbb08
Aditi Srivastava <117526497+Adiroki23@users.noreply.github.com>
2024-07-05 17:17:26 +0000

    maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,31 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+ 
+
+// ++++++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.9));
+console.log(Math.floor(4.2));
+console.log(Math.min(4, 9, 6, 7));
+console.log(Math.max(4, 9, 6, 7));
+
+console.log(Math.random());
+
+console.log(Math.random()*10);
