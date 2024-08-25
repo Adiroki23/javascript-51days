c21c791e41bd81a837717b8e979f06c57255b808
Aditi Srivastava <117526497+Adiroki23@users.noreply.github.com>
2024-08-25 19:41:46 +0000

    ways to print or call objects diffrently

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,62 @@
+// const tinderUser = new Object()       singleton 
+
+const tinderUser = {}
+
+tinderUser.id ="123abc"
+tinderUser.name ="aditi"
+tinderUser.isLoggedIn=false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "aditi",
+            lastname: "choudhary"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj3 = {5: "a", 6: "b"}
+
+
+//const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2)
+
+const obj4 = {...obj1, ...obj2, ...obj3}
+
+console.log(obj4);
+
+const users = [
+    {
+        id: 1,
+        email:"aditisrivas@gmail.com"
+    },
+    {
+        id: 1,
+        email:"aditisrivas@gmail.com"
+    },
+    {
+        id: 1,
+        email:"aditisrivas@gmail.com"
+    },
+    {
+        id: 1,
+        email:"aditisrivas@gmail.com"
+    },
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnPropery('isLoggedIn'));
+
