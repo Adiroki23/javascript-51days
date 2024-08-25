d434226738a102b626a62bef61eb7c153214abe5
Aditi Srivastava <117526497+Adiroki23@users.noreply.github.com>
2024-08-25 13:46:26 +0000

    floor and random

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -27,5 +27,12 @@ console.log(Math.min(4, 9, 6, 7));
 console.log(Math.max(4, 9, 6, 7));
 
 console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10)+1);
 
-console.log(Math.random()*10);
+const min =10
+const max =20
+
+console.log(Math.floor(Math.random() * (max - min +1)* min)) 
+
+
