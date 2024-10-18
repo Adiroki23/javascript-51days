fdba02b503b942dfa85159a47ca6d697ab2596c5
Aditi Srivastava <117526497+Adiroki23@users.noreply.github.com>
2024-10-18 21:44:59 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,67 @@
+// let a = 10
+// //const b = 20
+// //var c = 300
+
+// if (true) {
+
+//     let a = 10
+//     const b = 20
+//   //  var c = 30
+//     console.log("INNER: ", a);
+// }
+
+// for (let i = 0; i< array.length; i++){
+//     const element = array[i];
+
+
+// }
+
+// console.log(a);
+//  console.log(b);
+//  console.log(c);
+
+
+function one(){
+  const username = "aditi"
+
+  function two(){
+    const website = "youtube"
+    console.log(username);
+
+  }
+  // console.log(website);
+
+  two()
+
+}
+
+// one()
+
+
+
+if (true){
+  const username = " aditi"
+  if (username === " aditi"){
+    const website = "youtube"
+    // console.log(username + website);
+  }
+  console.log(website);
+
+}
+
+console.log(username);
+
+// ++++++++++++++++++++++++++intresting ++++++++++++++++++++++++++++++++++++
+
+
+function addone(num){
+  return num + 1
+}
+
+addone(6)
+
+const addTwo = function(num){
+  return num + 2
+}
+
+addTwo(7)
